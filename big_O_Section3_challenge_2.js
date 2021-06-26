39fc0ceac9e459fb6edced81117857804ee207a6
deog.amani@gmail.com <deog.amani@gmail.com>
2021-06-26 13:22:11 +0200

    find pairs in an array funciton

---
+++ big_O_Section3_challenge_2.js
@@ -0,0 +1,15 @@
+// log all pairs array
+const boxes = [1,2,3,4,5];
+
+//function to create pairs from an array
+function all_pairs_array(boxesarray){
+    let pairs=[];
+
+    for (let i = 0; i < boxesarray.length; i++){
+        for (let j = 0;)=[];{
+
+        }
+        pairs.push()
+    }
+
+}
