a16acec201a3dae0a43d036bf767ddf6cc5224b9
deog.amani@gmail.com <deog.amani@gmail.com>
2021-07-07 13:32:31 +0200

    Just made all conditions work for the fuction in a Big O of N

--- interviewquestion2_section4_exercise.js
+++ interviewquestion2_section4_exercise.js
@@ -1,71 +1,6 @@
-array1 = ['a','b','c','x'];
-array2 = ['z','y','i'];
-
-let common_items = (array1, array2) => {
-    
-    array1_pointer = 0;
-    array1_legnth = array1.length - 1;
-
-    array2_legnth = array2.length - 1;
-    array2_pointer = array2_legnth;
-    
-    while (True){
-        
-        if (array1[array1_pointer] == array2_pointer[array2_pointer]) return True; 
-
-        if ((array1[array1_pointer] < array2[array2_pointer]) && (array1_pointer < array1_legnth) ){
-            array1_pointer += 1;
-        }
-
-        if ((array1[array1_pointer] > array2[array2_pointer]) && (array2_pointer > 0) ){
-            array2_pointer -= 1;
-        }
-
-        if ((array1_pointer >= array1_legnth) | (array2_pointer <= 0) ){
-            return False;
-        }
-    } 
-};
-
-console.log(common_items)
-
-
-
-
-
 const array1 = ['a','b','c','x'];
 const array2 = ['z','y','i'];
-
-const common_items = (array1, array2) => {
-    
-    const array1_pointer = 0;
-    const array1_legnth = array1.length - 1;
-
-    const array2_legnth = array2.length - 1;
-    const array2_pointer = array2_legnth;
-    
-    while (true){
-        
-        if (array1[array1_pointer] == array2_pointer[array2_pointer]) return true; 
-
-        if ((array1[array1_pointer] < array2[array2_pointer]) && (array1_pointer < array1_legnth) ){
-            array1_pointer += 1;
-        }
-
-        if ((array1[array1_pointer] > array2[array2_pointer]) && (array2_pointer > 0) ){
-            array2_pointer -= 1;
-        }
-
-        if ((array1_pointer >= array1_legnth) | (array2_pointer <= 0) ){
-            return false;
-        }
-    } 
-};
-
-console.log(common_items.call(array1, array2));
-
-const array1 = ['a','b','c','x'];
-const array2 = ['z','y','i'];
+const array3 = ['z','y','x'];
 
 const common_items = (array1, array2) => {
     
@@ -89,6 +24,127 @@ const common_items = (array1, array2) => {
 
         if ((array1_pointer >= array1_legnth) | (array2_pointer <= 0) ){
             return false;
+        }
+    } 
+};
+
+console.log(common_items(array1, array2));
+
+
+
+
+const array1 = ['a','b','c','x'];
+const array2 = ['z','y','i'];
+const array3 = ['z','y','x'];
+
+const common_items = (array1, array2) => {
+    
+    let array1_pointer = 0;
+    let array1_legnth = array1.length -1;
+
+    let array2_legnth = array2.length - 1;
+    let array2_pointer = array2_legnth;
+    let terminate = 0;
+    
+    while (true){
+
+        if ((array1_pointer >= array1_legnth) | (array2_pointer <= 0) ){
+            console.log("ending");
+            return false;
+        }
+        
+        if (array1[array1_pointer] == array2[array2_pointer]){return true;} 
+
+        if ((array1[array1_pointer] < array2[array2_pointer]) && (array1_pointer < array1_legnth) ){
+            console.log('array1 ' + array1[array1_pointer]);
+            array1_pointer += 1;
+            console.log('array1 ' + array1[array1_pointer]);
+        }
+
+        if ((array1[array1_pointer] > array2[array2_pointer]) && (array2_pointer > 0) ){
+            console.log('array2 ' + array2[array2_pointer]);
+            array2_pointer -= 1;
+            console.log('array2 ' + array2[array2_pointer]);
+        }
+    } 
+};
+
+console.log(common_items(array1, array2));
+
+
+// had a bug of not having a working if condition
+
+const array1 = ['a','b','c','x'];
+const array3 = ['z','y','i'];
+const array2 = ['z','y','x'];
+
+const common_items = (array1, array2) => {
+    
+    let array1_pointer = 0;
+    let array1_legnth = array1.length;
+
+    let array2_legnth = array2.length - 1;
+    let array2_pointer = array2_legnth;
+    let terminate = 0;
+    
+    while (true){
+
+        if ((array1_pointer > array1_legnth) | (array2_pointer < 0) ){
+            console.log("ending");
+            return false;
+        }
+        
+        if (array1[array1_pointer] == array2[array2_pointer]){return true;} 
+
+        if ((array1[array1_pointer] < array2[array2_pointer]) && (array1_pointer < array1_legnth) ){
+            console.log('array1 ' + array1[array1_pointer]);
+            array1_pointer += 1;
+        }
+
+        if ((array1[array1_pointer] > array2[array2_pointer]) && (array2_pointer > 0) ){
+            console.log('array2 ' + array2[array2_pointer]);
+            array2_pointer -= 1;
+            console.log('array2 ' + array2[array2_pointer]);
+        }
+    } 
+};
+
+console.log(common_items(array1, array2));
+
+
+
+
+
+
+// working function
+const array1 = ['a','b','c','x'];
+const array2 = ['z','y','i'];
+
+const array3 = ['z','y','x'];
+
+const common_items = (array1, array2) => {
+    
+    let array1_pointer = 0;
+    let array1_legnth = array1.length - 1;
+
+    let array2_legnth = array2.length - 1;
+    let array2_pointer = array2_legnth;
+    
+    while (true){
+
+        if ((array1_pointer > array1_legnth) | (array2_pointer < 0) ){
+            console.log("ending");
+            return false;
+        }
+        
+        if (array1[array1_pointer] == array2[array2_pointer]){return true;} 
+
+        if ((array1[array1_pointer] < array2[array2_pointer]) && (array1_pointer < array1_legnth+1) ){
+            array1_pointer += 1;
+        }
+
+        if ((array1[array1_pointer] > array2[array2_pointer]) && (array2_pointer > -1) ){
+            array2_pointer -= 1;
         }
     } 
 };
