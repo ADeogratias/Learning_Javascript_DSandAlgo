1688117f8a70377de4b1d58f6ae21d6ed754a292
deog.amani@gmail.com <deog.amani@gmail.com>
2021-07-17 18:42:02 +0200

    done and going to push and start working on the next section and using classes

--- interviewquestion2_section4_exercise.js
+++ interviewquestion2_section4_exercise.js
@@ -32,6 +32,51 @@
 // a being size of array1 and b being size of array2
 // Big O (1) - space complexity 
 
+
+// had a bug of not having a working if condition
+// this function would get to a point during the testing
+// were the while conditions continues running, 
+// but it is not meeting any if condition... 
+
+//const array1 = ['a','b','c','x'];
+//const array3 = ['z','y','i'];
+//const array2 = ['z','y','x'];
+
+//const common_items = (array1, array2) => {
+    
+    //let array1_pointer = 0;
+    //let array1_legnth = array1.length;
+
+    //let array2_legnth = array2.length - 1;
+    //let array2_pointer = array2_legnth;
+    //let terminate = 0;
+    
+   // while (true){
+
+     //   if ((array1_pointer > array1_legnth) | (array2_pointer < 0) ){
+       //     console.log("ending");
+         //   return false;
+        //}
+        
+        //if (array1[array1_pointer] == array2[array2_pointer]){return true;} 
+
+    //    if ((array1[array1_pointer] < array2[array2_pointer]) && (array1_pointer < array1_legnth) ){
+      //      console.log('array1 ' + array1[array1_pointer]);
+        //    array1_pointer += 1;
+        //}
+
+    //    if ((array1[array1_pointer] > array2[array2_pointer]) && (array2_pointer > 0) ){
+      //      console.log('array2 ' + array2[array2_pointer]);
+        //    array2_pointer -= 1;
+          //  console.log('array2 ' + array2[array2_pointer]);
+        //}
+    //} 
+//};
+
+//console.log(common_items(array1, array2));
+
+
+
 // working function
 const array1 = ['a','b','c','x'];
 const array2 = ['z','y','i'];
@@ -131,46 +176,3 @@ containsCommonItem2 = (array1, array2) =>{
 
 // then you talk about modularizing your codes.
 // having a function that does one particular task and does it very well.
-
-
-// had a bug of not having a working if condition
-// this function would get to a point during the testing
-// were the while conditions continues running, 
-// but it is not meeting any if condition... 
-
-//const array1 = ['a','b','c','x'];
-//const array3 = ['z','y','i'];
-//const array2 = ['z','y','x'];
-
-//const common_items = (array1, array2) => {
-    
-    //let array1_pointer = 0;
-    //let array1_legnth = array1.length;
-
-    //let array2_legnth = array2.length - 1;
-    //let array2_pointer = array2_legnth;
-    //let terminate = 0;
-    
-   // while (true){
-
-     //   if ((array1_pointer > array1_legnth) | (array2_pointer < 0) ){
-       //     console.log("ending");
-         //   return false;
-        //}
-        
-        //if (array1[array1_pointer] == array2[array2_pointer]){return true;} 
-
-    //    if ((array1[array1_pointer] < array2[array2_pointer]) && (array1_pointer < array1_legnth) ){
-      //      console.log('array1 ' + array1[array1_pointer]);
-        //    array1_pointer += 1;
-        //}
-
-    //    if ((array1[array1_pointer] > array2[array2_pointer]) && (array2_pointer > 0) ){
-      //      console.log('array2 ' + array2[array2_pointer]);
-        //    array2_pointer -= 1;
-          //  console.log('array2 ' + array2[array2_pointer]);
-        //}
-    //} 
-//};
-
-//console.log(common_items(array1, array2));
