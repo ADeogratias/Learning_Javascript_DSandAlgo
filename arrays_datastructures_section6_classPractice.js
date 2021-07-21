0601c6c30923acabac83700eb162a6fdc2a0f30d
ADeogratias <deog.amani@gmail.com>
2021-07-21 13:05:27 +0200

    working on reference types first

---
+++ arrays_datastructures_section6_classPractice.js
@@ -0,0 +1,21 @@
+// ******* reference type vs primitive types ****
+
+// Primitive types are the basic types 
+// of data: byte , short , int , long , float , double , boolean , char  
+// Primitive variables store primitive values. 
+// Reference types are any instantiable class 
+// as well as arrays: String , Scanner , Random , Die , int[] , String[] , etc. 
+// in Java for instance
+
+// reference types
+[] === [] // will basically give you a false result
+
+var object1 = {value: 10};
+var object2 = object1;
+var object3 = {value: 10};
+
+
+
+// context vs scope 
+
+//  instantiation
