f930aa608879187808c12b533167d8b3b8e421c9
ADeogratias <deog.amani@gmail.com>
2021-07-21 13:23:49 +0200

    finished working on reference type. Moving on to context vs scope

--- arrays_datastructures_section6_classPractice.js
+++ arrays_datastructures_section6_classPractice.js
@@ -1,4 +1,4 @@
-// ******* reference type vs primitive types ****
+// ******* reference type vs primitive types *******
 
 // Primitive types are the basic types 
 // of data: byte , short , int , long , float , double , boolean , char  
@@ -8,14 +8,36 @@
 // in Java for instance
 
 // reference types
+
+// Example 1
 [] === [] // will basically give you a false result
 
+// Example 2
 var object1 = {value: 10};
 var object2 = object1;
 var object3 = {value: 10};
 
+object1 === object2; // will return True
+object2 === object3; // return false
+
+object1.value === object3.value; // return true
+
+// numbers, null, booleans, strings, undefined and symbols 
+// are defined by the programming language 
+// example var number1 = 1 Javascript knows 
+// what 1 is its a number and it's a primitve type
 
 
-// context vs scope 
+// Objects are whats called the reference types in javascript.
+// a reference type, which is a non-premitive type are not 
+// defined by the programming language
+// They are created by the programmer
+// example var object 1 = {value: 10}; this stores the reference/address 
+// of where the value is stored in the memory
+
+
+
+// ******* context vs scope *******
+
 
 //  instantiation
