23538f5c796fbe34bf57348ca1802b80b9b8385d
ADeogratias <deog.amani@gmail.com>
2021-07-23 04:42:01 +0200

    just finished implementing the function to reverse the string

--- arrays_datastructures_section6_classPractice3.js
+++ arrays_datastructures_section6_classPractice3.js
@@ -4,5 +4,20 @@
 // 'ierdnA si eman iH'
 
 function myReversefunc(str) {
-    
+    let string2Reverse = str.split('');
+    string2Reverse.reverse()
+    console.log(string2Reverse);
+    let strCount = string2Reverse.length -1;
+    let reversedString;
+    while (strCount <= 0) {
+        reversedString = reversedString + string2Reverse[strCount];
+        strCount--;
+    }
+    return reversedString;
 }
+
+// Trying to improve the function above for readability
+
+let myReversefunc2 = (str) => {
+    
+};
