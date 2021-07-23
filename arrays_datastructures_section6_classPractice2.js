9cdd128c2b5d1bce2589b362aa1cd2b53a8efe98
ADeogratias <deog.amani@gmail.com>
2021-07-23 04:21:07 +0200

    reversing a string exercise

--- arrays_datastructures_section6_classPractice2.js
+++ arrays_datastructures_section6_classPractice2.js
@@ -1,3 +1,58 @@
 // How to build an array
 // and use it
 
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+
+    //get item at index
+    get(index) {
+        return this.data[index];
+    }
+
+    //push item in array last index
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+
+    // remove array item
+    pop() {
+        const lastItem = this.data[this.length-1];
+        delete this.data[this.length-1];
+        this.length--;
+        return lastItem;
+    }
+
+    // delete item at a particular index
+    delete(index) {
+        const item = this.data[index];
+        this.shiftItems(index);
+        return item;
+    }
+
+    shiftItems(startIndex){
+        for (let index = startIndex; index < this.length - 1; index++)
+        {
+            this.data[index] = this.data[index+1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
+    }
+}
+
+const newArray = new MyArray();
+newArray.push('hi');
+newArray.push('you');
+newArray.push('!');
+// newArray.pop();
+// newArray.pop();
+newArray.delete(1);
+newArray.push('play');
+newArray.push('nice');
+newArray.delete(0);
+newArray.delete(0);
+console.log(newArray);
---
+++ arrays_datastructures_section6_classPractice3.js
@@ -0,0 +1,8 @@
+// Reversing a string exercise
+// Create a function that reverses a string:
+// 'Hi My name is Andrei' should be:
+// 'ierdnA si eman iH'
+
+function myReversefunc(str) {
+    
+}
