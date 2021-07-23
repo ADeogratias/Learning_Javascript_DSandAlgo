f004e23dbb869024c916ac32c7f4c1878e6f92f6
ADeogratias <deog.amani@gmail.com>
2021-07-23 10:18:16 +0200

    changed file name, and now about to attempt doiing a merge sort

---
+++ arrays_datastructures_section6_mergeSortedArrays4.js
@@ -0,0 +1,11 @@
+// Given 2 arrays that are sorted
+// merge them together into 1 big 
+// one that is still sorted
+
+mergSortedArrays([0,3,4,31], [4,6,30]);
+// [0,3,4,4,6,30,31]
+
+// join the 2 arrays in 1 big arrays
+// sort the array using mergesort
+
+// merge sort implementation
