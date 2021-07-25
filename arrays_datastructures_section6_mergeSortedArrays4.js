3dc730ac7f0c50b1cdaacd923fe15022e8766962
ADeogratias <deog.amani@gmail.com>
2021-07-25 13:12:15 +0200

    done merging the 2 sorted arrays

--- arrays_datastructures_section6_mergeSortedArrays4.js
+++ arrays_datastructures_section6_mergeSortedArrays4.js
@@ -35,23 +35,77 @@ const mergeSortedArrays = (array1, array2) => {
         let item1 = array1[leftPointer];
         let item2 = array2[rightPointer]; 
 
-        if ( isGreater(item1, item2)){
+        if ( isGreater(item1, item2) & isGreater(maxCount, leftPointer)){
             mergedSortedArray.push(item1);
             leftPointer++;
         }
-        else{
-            mergedSortedArray.push(item2);
-            rightPointer++;
+        else {
+            if ( isGreater(array2Length, rightPointer)){
+                mergedSortedArray.push(item2);
+                rightPointer++;
+            }
         }
 
-        if (isGreater(rightPointer, maxCount)){
+        // if (isGreater(rightPointer, maxCount)){
 
-        }
+        // }
     }
 };
 
 
-const isGreater = (item1, item2) => item1 >= item2;
+const isGreater = (item1, item2) => item1 > item2;
 
 
-// to be continueed...
+// to be continueed...n
+
+
+const isGreater = (item1, item2) => item1 > item2;
+
+const mergeSortedArrays = (array1, array2) => {
+    let mergedSortedArray = [];
+    let maxCount = array1.length;
+    let array2Length = array2.length;
+    
+    console.log ("array 1 length: " + maxCount);
+    console.log ("array 2 length: " + array2Length);
+
+    let leftPointer = 0;
+    let rightPointer = 0;
+    // let count = 0;
+    if (maxCount < array2Length) {
+        maxCount = array2Length;
+    }
+    while(rightPointer < array2Length | leftPointer < maxCount){
+        
+        let item1 = array1[leftPointer];
+        let item2 = array2[rightPointer]; 
+
+        // if (isGreater(maxCount === leftPointer & ))
+
+        if ( isGreater(item2, item1) | rightPointer === array2Length ){
+            mergedSortedArray.push(item1);
+            console.log ("array 1 received: " + item1);
+            console.log ("LeftPointer before: " + leftPointer);
+            leftPointer++;
+            console.log ("after: " + leftPointer);
+
+            // if (isGreater(maxCount, leftPointer)){
+            //     leftPointer++;
+            // }
+
+        }
+        else {
+            mergedSortedArray.push(item2);
+            console.log ("array 2 received: " + item2);
+            console.log ("RightPointer before: " + rightPointer);
+            rightPointer++;
+            console.log ("after: " + rightPointer);
+            // if ( isGreater(array2Length, rightPointer)){
+            //     rightPointer++;
+            // }
+        }
+    }
+    return mergedSortedArray;
+};
+
+console.log(mergeSortedArrays([0,3,4,31], [4,6,30]));
