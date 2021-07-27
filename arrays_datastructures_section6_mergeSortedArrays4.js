68e02d7697f2b0835ec17887e6e5b3bc770c9ac4
ADeogratias <deog.amani@gmail.com>
2021-07-28 01:44:25 +0200

    adding the solution from the video

--- arrays_datastructures_section6_mergeSortedArrays4.js
+++ arrays_datastructures_section6_mergeSortedArrays4.js
@@ -95,4 +95,28 @@ console.log(mergeSortedArrays([0,3,4,31], [4,6,30]));
 // const isGreater = (item1, item2) => item1 > item2;
 
 
+// How the solution from the video solves the problem
+// Solution from Andrei
 
+function mergedSortedArrays(array1, array2) {
+    const megerdArray = [];
+    let array1Item = array1[0];
+    let array2Item = array2[0];
+
+    //Check input
+    if (array1.length === 0) {
+        return array2;
+    }
+
+    if (array2.length === 0) {
+        return array1;
+    }
+
+    while (array1Item || array2Item) {
+        
+    }
+
+    return megerdArray;
+}
+
+mergeSortedArrays([0,3,4,31], [4,6,30]);
